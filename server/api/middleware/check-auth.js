bbaee3e8cac152cf50d01662fe984c0f75b00eef
Shreyansh <shreyansh.malviya2020@vitbhopal.ac.in>
2022-08-29 16:48:11 +0530

    established authentication middleware

---
+++ server/api/middleware/check-auth.js
@@ -0,0 +1,22 @@
+module.exports = (req, res, next) => {
+    if (!req.headers.authorization) {
+        return res.status(401).json({
+            message: 'Unauthorized request'
+        });
+    }
+    const token = req.headers.authorization.split(' ')[1];
+    if (token === 'null') {
+        return res.status(401).json({
+            message: 'Unauthorized request'
+        });
+    }
+    const payload = jwt.verify(token, `${process.env.JWT_KEY}`);
+    if (!payload) {
+        return res.status(401).json({
+            message: 'Unauthorized request'
+        });
+    }
+    console.log(payload)
+    req.userId = payload.subject;
+    next();
+}
