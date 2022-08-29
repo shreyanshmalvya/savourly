2f0e5ff256038277d21f78e333c8e205aff24fd0
Shreyansh <shreyansh.malviya2020@vitbhopal.ac.in>
2022-08-29 18:28:26 +0530

    updated usermodel to store username

--- server/api/model/user.js
+++ server/api/model/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     _id :  mongoose.Schema.Types.ObjectId,
+    username: { type: String, required: true },
     email : {type  : String , required  : true, unique: true, match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/},
     password : {type : String, required : true}
 });
