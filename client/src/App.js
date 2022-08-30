18ee0a7ab964c3741279f24c189bc525964258d7
Shreyansh <shreyansh.malviya2020@vitbhopal.ac.in>
2022-08-30 05:54:10 +0530

    added router

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import { Home } from './pages/home/Home';
+import ReadPage from './pages/read/ReadPage';
+import Login from './pages/login/Login';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Dashboard from './pages/dashboard/Dashboard';
 
 function App() {
   return (
     <div className="App">
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/read/" element={<ReadPage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
